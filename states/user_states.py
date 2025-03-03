d0459058dcdd97a13b27b82303a91c44be57237d
holost-m <m.holostov@yandex.ru>
2025-03-04 01:27:57 +0300

    Написал FSM

--- states/user_states.py
+++ states/user_states.py
@@ -1,9 +1,22 @@
 import redis
-import json
+from copy import copy
 
 red = redis.Redis(host='localhost', port=6379, db=0)
 
 class FSM:
+    initial_hash: dict = {'number': '1',
+                              '1': '0',
+                              '2': '0',
+                              '3': '0',
+                              '4': '0',
+                              '5': '0'}
+
+    @staticmethod
+    def __decoder(redis_dict: dict) -> dict:
+        """
+        Декодируем байтовые значения словаря, полученного из redis
+        """
+        return {key.decode('utf-8'): value.decode('utf-8') for key, value in redis_dict.items()}
 
     @classmethod
     def init_state(cls, tg_id):
@@ -11,7 +24,8 @@ class FSM:
         Инициализация пользователя.
         Он зашел в первый раз
         """
-        red.hset(str(tg_id), 'number', '1')
+        red.hset(str(tg_id),
+                 mapping=FSM.initial_hash)
 
     @classmethod
     def get_number(cls, tg_id):
@@ -21,29 +35,40 @@ class FSM:
         number = red.hget(str(tg_id), 'number')
         if number:
             number = int(number)
-        return
+        return number
 
     @classmethod
     def restore(cls, tg_id, number):
         """
         Восстановит состояние пользователя до то того как он вышел
         """
+        current_hash = copy(FSM.initial_hash)
+        current_hash['number'] = str(number)
 
-        red.hset(str(tg_id), 'number', '1')
+        red.hset(str(tg_id),
+                 mapping=current_hash)
 
     @classmethod
-    def next_state(cls):
+    def next_state(cls, tg_id):
         """
         Переход на следующий вопрос.
         Обнуление ответов - приориетов
         """
-        pass
+        number = int(red.hget(str(tg_id), 'number'))
+        number += 1
+        current_hash = copy(FSM.initial_hash)
+        current_hash['number'] = str(number)
+
+        red.hset(str(tg_id),
+                 mapping=current_hash)
 
     @classmethod
-    def get_priority(cls):
+    def get_data(cls, tg_id):
         """
-        Возвращение текущего словаря приоритетов
+        Возвращение текущего словаря
         """
+        result = red.hgetall(str(tg_id))
+        return cls.__decoder(result)
 
     @classmethod
     def set_priority(cls, tg_id, key, priority):
@@ -51,3 +76,7 @@ class FSM:
         Установит текущий приоритет
         """
         red.hset(str(tg_id), str(key), str(priority))
+
+    @classmethod
+    def has_fsm(cls, tg_id) -> bool:
+        return red.exists(str(tg_id))
